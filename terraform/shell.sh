67b65482bd4437e6a2d65a9b2e641ee8428774a5
SathwikReddyM <88212513+SathwikReddyM@users.noreply.github.com>
2023-06-06 21:28:27 -0700

    Update shell.sh

--- terraform/shell.sh
+++ terraform/shell.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+RDS_ENDPOINT=${rds_endpoint}
 sudo apt update
 sudo apt install -y docker.io
 
 git clone https://github.com/SathwikReddyM/terrafom.git
 
 cd terrafom
-
+echo "endpoint = '$RDS_ENDPOINT'" > details.py
 sudo docker build -t myflask .
-sudo docker run -d -p 5000:5000 --name myflask_container myflask
+
 
 
 sudo apt-get update
@@ -19,16 +19,16 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 
 # Edit the default Nginx configuration file
-sudo nano -ES /etc/nginx/sites-available/default <<EOF
-upstream flaskhelloworld {
-    server 127.0.0.1:5000;
-}
+# Append the upstream block to the configuration file
+echo "upstream flaskhelloworld {" >> /etc/nginx/sites-available/default
+echo "    server 127.0.0.1:5000;" >> /etc/nginx/sites-available/default
+echo "}" >> /etc/nginx/sites-available/default
 
-location / {
-    proxy_pass http://flaskhelloworld;
-}
-EOF
-
+# Append the location directive to the configuration file
+echo "location / {" >> /etc/nginx/sites-available/default
+echo "    proxy_pass http://flaskhelloworld;" >> /etc/nginx/sites-available/default
+echo "}" >> /etc/nginx/sites-available/default
+sudo docker run -d -p 5000:5000 --name myflask_container myflask
 # Restart Nginx
 sudo systemctl restart nginx
 
